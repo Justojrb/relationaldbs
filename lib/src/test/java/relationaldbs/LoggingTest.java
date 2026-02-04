6d3c1fc866a0afbc2391c4472702f6fe53d81d54
justoj <justojrb05@gmail.com>
2026-02-04 13:00:50 +0100

    Libraries implementation

---
+++ lib/src/test/java/relationaldbs/LoggingTest.java
@@ -0,0 +1,15 @@
+package relationaldbs;
+
+import org.slf4j.Logger;
+
+public class LoggingTest {
+	public static void main(String[] args) {
+		Logger logger = null;
+		logger.trace(null);
+		logger.debug(null);
+		logger.info("");
+		logger.warn(null);
+		logger.error(null);
+	}
+
+}
