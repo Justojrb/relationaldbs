3f117b0901a6a2cca5d30f66c877539632811cbc
justoj <justojrb05@gmail.com>
2026-03-03 12:59:05 +0100

    Log implementation

--- lib/src/test/java/relationaldbs/LoggingTest.java
+++ lib/src/test/java/relationaldbs/LoggingTest.java
@@ -1,15 +1,46 @@
 package relationaldbs;
 
-import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+
 
 public class LoggingTest {
+	
+	
+private final static Logger logger = (Logger) LoggerFactory.getLogger(LoggingTest.class); 
 	public static void main(String[] args) {
-		Logger logger = null;
-		logger.trace(null);
-		logger.debug(null);
-		logger.info("");
-		logger.warn(null);
-		logger.error(null);
+//      Logger.setLeveL(Level.TRACE);
+//	    Logger.setLeveL(Level.DEBUG);
+//	    Logger.setLeveL(Level.INFO);
+//	    Logger.setLeveL(Level.WARN);
+//	    Logger.setLeveL(Level.ERROR);
+		logger.setLevel(Level.INFO);
+		division(20, 0);
 	}
-
+	
+	private static void division(int i, int j) {
+		//this level of information has less importance 
+		logger.trace("trace info-very detailed info: You have entered the method division");
+		//We use this level of information for test 
+		logger.debug("test info: division method test");
+		//We use this level of log for normal and general information 
+		logger.info("main general info to show our app is running normally");
+		//We use this level of log for potential problems 
+		logger.warn("unexpected problem but not vital");
+		//This is for errors 
+		logger.error("serious problem that should be resolved");
+		if(j== 0) {
+			logger.info("the user has pass a zero as divisor");
+		}
+		try {
+			int result= i/j;
+			logger.info("the result is :" + result);
+			return;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
